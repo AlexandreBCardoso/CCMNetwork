874410bb27cb474cd2f7171cc5bb67caece1c592
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2023-08-30 21:12:55 -0300

    feat(1): Cria mapper para converter em URLRequest

--- CCMNetwork/Sources/CCMNetwork/NetworkRequestMapper.swift
+++ CCMNetwork/Sources/CCMNetwork/NetworkRequestMapper.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct NetworkRequestMapper {
+
+    static func map(from request: NetworkRequest) -> URLRequest? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = request.scheme
+        urlComponents.host = request.host
+        urlComponents.path = request.path
+        urlComponents.queryItems = request.queryItems?.compactMap { key, value in
+            URLQueryItem(name: key, value: value)
+        }
+        
+        if let url = urlComponents.url {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.allHTTPHeaderFields = request.headers
+            urlRequest.httpMethod = request.method.rawValue
+            urlRequest.httpBody = request.body
+            return urlRequest
+        } else {
+            return nil
+        }
+    }
+
+}
